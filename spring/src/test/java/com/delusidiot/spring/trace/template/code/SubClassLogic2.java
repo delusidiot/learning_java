c7c6e36dd93321415d4c8b65614b14d617c7b4e8
delusidiot <delusidiot@gmail.com>
2022-08-17 23:36:15 +0900

    [spring framework] TemplateMethod Pattern 사용

---
+++ spring/src/test/java/com/delusidiot/spring/trace/template/code/SubClassLogic2.java
@@ -0,0 +1,16 @@
+package com.delusidiot.spring.trace.template.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * author       : delusidiot
+ * date         : 2022-08-16
+ */
+@Slf4j
+public class SubClassLogic2 extends AbstractTemplate {
+
+    @Override
+    protected void call() {
+      log.info("비즈니스 로직 2");
+    }
+}
