c10d956a97a58628e020e63e17f9faa88d9173ea
delusidiot <delusidiot@gmail.com>
2023-05-16 09:46:03 +0900

    [JavaEE(JakartaEE)] Initial commit

---
+++ JavaEEServlet/README.md
@@ -0,0 +1,8 @@
+# Java Dynamic Web Application
+
+Oracle 은 2017년 JavaEE 8 릴리즈를 마지막으로 Eclipse 재단에 JavaEE 프로젝트를 이관했습니다.
+Eclipse 재단으로 이관된 JavaEE의 공식 명칭은 JakartaEE, 프로젝트 명은 EE4J(Eclipse Enterprise for Java)로 변경되었습니다.
+Oracle 이 JavaEE 프로젝트는 이관했지만 Java 상표권은 여전히 보유하고 있기 때문에 Java 네임스페이스 사용에 제약이 있어 JakartaEE 에서 javax.*에서 Jakarta.* 로 변경되었습니다.
+
+## Tomcat
+Jakarta 를 사용하기 위해 Tomcat 10버전 이상을 사용해야 합니다. 이전 JavaEE를 사용하는 경우 Tomcat 9이하를 사용하면 됩니다.
---
+++ JavaEEServlet/src/main/java/com/delusidiot/javaeeservlet/FirstServlet.java
@@ -0,0 +1,25 @@
+package com.delusidiot.javaeeservlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class FirstServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        System.out.println("init");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doGet");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy");
+    }
+}
