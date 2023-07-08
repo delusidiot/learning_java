e9c8e5d20e465ac25fb67aaadb6c037d91d6c03a
delusidiot <delusidiot@gmail.com>
2023-07-08 13:22:33 +0900

    [Spring Security] Embedded H2 database 와 연결

--- spring-security-tutorial/src/main/java/com/example/springsecuritytutorial/config/SecurityConfiguration.java
+++ spring-security-tutorial/src/main/java/com/example/springsecuritytutorial/config/SecurityConfiguration.java
@@ -2,9 +2,18 @@ package com.example.springsecuritytutorial.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -15,12 +24,27 @@ public class SecurityConfiguration {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> {
-                    authorize.requestMatchers("/").permitAll();
-                    authorize.requestMatchers("/user/**").authenticated();
-                    authorize.requestMatchers("/admin/**").denyAll();
+                    authorize.requestMatchers(HttpMethod.GET, "/").permitAll();
+                    authorize.requestMatchers(HttpMethod.GET, "/user/**").hasRole("USER");
+                    authorize.requestMatchers(HttpMethod.GET, "/admin/**").hasRole("ADMIN");
+                    authorize.anyRequest().authenticated();
                 })
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(header-> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .formLogin(withDefaults())
                 .httpBasic(withDefaults());
         return http.build();
+    }
+    @Bean
+    JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
     }
 }
