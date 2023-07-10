84e96b9e05a19c6e765f9a1fa5ebe4e1b458f50e
delusidiot <delusidiot@gmail.com>
2023-07-10 14:30:21 +0900

    [AuthorizationServer] initial commit

---
+++ spring-security-oauth2-tutotial/src/main/java/com/example/springsecurityoauth2tutotial/SpringSecurityOauth2TutotialApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityoauth2tutotial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauth2TutotialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityOauth2TutotialApplication.class, args);
+    }
+
+}
---
+++ spring-security-oauth2-tutotial/src/test/java/com/example/springsecurityoauth2tutotial/SpringSecurityOauth2TutotialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityoauth2tutotial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauth2TutotialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
