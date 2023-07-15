d21d92f5d2058873345080194494ac70201872dc
delusidiot <delusidiot@gmail.com>
2023-07-15 23:36:15 +0900

    [Security-JWT] Roles and Permissions Based Authorization

--- spring-security-jwt-demo/src/main/java/com/example/springsecurityjwtdemo/SpringSecurityJwtDemoApplication.java
+++ spring-security-jwt-demo/src/main/java/com/example/springsecurityjwtdemo/SpringSecurityJwtDemoApplication.java
@@ -1,7 +1,15 @@
 package com.example.springsecurityjwtdemo;
 
+import com.example.springsecurityjwtdemo.user.User;
+import com.example.springsecurityjwtdemo.user.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static com.example.springsecurityjwtdemo.user.Role.ADMIN;
+import static com.example.springsecurityjwtdemo.user.Role.MANAGER;
 
 @SpringBootApplication
 public class SpringSecurityJwtDemoApplication {
@@ -10,4 +18,30 @@ public class SpringSecurityJwtDemoApplication {
         SpringApplication.run(SpringSecurityJwtDemoApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder
+    ) {
+        return args -> {
+            userRepository.save(
+                    User.builder()
+                            .firstname("ADMIN")
+                            .lastname("😊")
+                            .email("admin@test.com")
+                            .password(passwordEncoder.encode("1234"))
+                            .role(ADMIN)
+                            .build()
+            );
+            userRepository.save(
+                    User.builder()
+                            .firstname("MANAGER")
+                            .lastname("😊")
+                            .email("manager@test.com")
+                            .password(passwordEncoder.encode("1234"))
+                            .role(MANAGER)
+                            .build()
+            );
+        };
+    }
 }
--- spring-security-jwt-demo/src/main/java/com/example/springsecurityjwtdemo/config/SecurityConfiguration.java
+++ spring-security-jwt-demo/src/main/java/com/example/springsecurityjwtdemo/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,9 +14,15 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 
+import static com.example.springsecurityjwtdemo.user.Permission.*;
+import static com.example.springsecurityjwtdemo.user.Role.ADMIN;
+import static com.example.springsecurityjwtdemo.user.Role.MANAGER;
+import static org.springframework.http.HttpMethod.*;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
@@ -28,6 +35,19 @@ public class SecurityConfiguration {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> {
                     authorize.requestMatchers("/api/auth/**").permitAll();
+
+                    authorize.requestMatchers("/api/management/**").hasAnyRole(ADMIN.name(), MANAGER.name());
+                    authorize.requestMatchers(GET, "/api/management/**").hasAnyAuthority(ADMIN_READ.name(), MANAGER_READ.name());
+                    authorize.requestMatchers(POST, "/api/management/**").hasAnyAuthority(ADMIN_CREATE.name(), MANAGER_CREATE.name());
+                    authorize.requestMatchers(PUT, "/api/management/**").hasAnyAuthority(ADMIN_UPDATE.name(), MANAGER_UPDATE.name());
+                    authorize.requestMatchers(DELETE, "/api/management/**").hasAnyAuthority(ADMIN_DELETE.name(), MANAGER_DELETE.name());
+
+//                    authorize.requestMatchers("/api/admin/**").hasRole(ADMIN.name());
+//                    authorize.requestMatchers(GET, "/api/admin/**").hasAuthority(ADMIN_READ.name());
+//                    authorize.requestMatchers(POST, "/api/admin/**").hasAuthority(ADMIN_CREATE.name());
+//                    authorize.requestMatchers(PUT, "/api/admin/**").hasAuthority(ADMIN_UPDATE.name());
+//                    authorize.requestMatchers(DELETE, "/api/admin/**").hasAuthority(ADMIN_DELETE.name());
+
                     authorize.anyRequest().authenticated();
                 })
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
